38e3dfa740dc36d0203e225090ea028ca4e96904
TimZoet <ts.zoet@gmail.com>
2023-03-26 14:54:53 +0200

    Inject callback into Conan graph building.

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import argparse
 from conan.api.conan_api import ConanAPI
+from conans.model.requires import Requirement
 import os
 import subprocess
 
@@ -10,29 +11,34 @@ if __name__ == "__main__":
     parser.add_argument("--directory", required=True)
     args = parser.parse_args()
 
+    visited = []
+
+    def callback(require: Requirement):
+        if require.ref.user != "timzoet":
+            return
+        if require.ref.name in visited:
+            return
+        visited.append(require.ref.name)
+        
+        path = os.path.join(args.directory, require.ref.name)
+        subprocess.run([
+            "git", "clone", "--depth", "1",
+            "-b", require.ref.channel,
+            "--recurse-submodules",
+            f"https://github.com/TimZoet/{require.ref.name}.git",
+            path
+        ], check=True)
+        subprocess.run([
+            "conan", "export",
+            "--user", require.ref.user,
+            "--channel", require.ref.channel,
+            path
+        ], check=True)
+
     api = ConanAPI()
     profile = api.profiles.get_profile([args.profile])
     deps_graph = api.graph.load_graph_consumer(args.conanfile, None, None,
                                                None, None,
                                                profile, profile, None,
-                                               [], [], [])
-    
-    for node in deps_graph.nodes[1:]:
-        if node.conanfile.user != "timzoet":
-            continue
-        
-        path = os.path.join(args.directory, node.conanfile.name)
-        
-        subprocess.run([
-            "git", "clone", "--depth", "1",
-            "-b", node.conanfile.channel,
-            f"https://github.com/TimZoet/{node.conanfile.name}.git",
-            path
-        ], check=True)
-
-        subprocess.run([
-            "conan", "export",
-            "--user", node.conanfile.user,
-            "--channel", node.conanfile.channel,
-            path
-        ], check=True)
+                                               [], [], False, False,
+                                               callback)
